f49a216c6257f8dece0cef6512800eb9bc9ce011
JGWill Gaia.WSL <jgi@jgwill.com>
2022-04-01 06:42:58 -0400

    regen

--- ico/_index_generator.sh
+++ ico/_index_generator.sh
@@ -1,1 +1,2 @@
-(echo "# /ico index" ;echo "----";echo " ";echo '|    |    |' ; echo '| --- |  --- |';for f in *png *svg *jpg; do ff=${f%.*};echo "|[$ff]("$f")"' | !['$ff']('$f') |'; done ) > index.md
+style='<style>img { max-width: 50px;max-height: 50px; } </style>'
+(echo "$style";echo "# /ico index" ;echo "----";echo " ";echo '|    |    |' ; echo '| --- |  --- |';for f in *png *svg *jpg; do ff=${f%.*};echo "|[$ff]("$f")"' | [!['$ff']('$f')]('$f') |'; done ) > index.md
